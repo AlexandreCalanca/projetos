a9d546e206c971ff40d8584ac40cbb639454f100
Alexandre Calanca <alexandre.ferreira@BMART.LOCAL>
2026-04-22 10:55:35 -0300

    Criei o desafio 2 e ja criei as classes

---
+++ desafio2/untitled/src/entities/Athletes.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Athletes {
+    private String name;
+    private Character gender;
+    private Double height;
+    private Double weigth;
+
+    //constructor
+    public Athletes(){
+    }
+    public Athletes(String name, Character gender, Double height, Double weight){
+        this.name = name;
+        this.gender = gender;
+        this.height = height;
+        this.weigth = weight;
+    }
+
+    //getters and setters
+
+    //methods
+    public Double averageWeight(){
+        return ;
+    }
+    public String tallestAthlete(){
+        return ;
+    }
+    public Double percentageOfMen(){
+        return ;
+    }
+    public Double averageFemaleHeight(){
+        return ;
+    }
+}
